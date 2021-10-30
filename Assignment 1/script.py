57dffda6b4c6d9458041b25a9c10573cda34cd0e
Daniel Bahrami <daniel@bahrami.dk>
2021-10-30 18:57:52 +0200

    Solution

---
+++ Assignment 1/script.py
@@ -0,0 +1,35 @@
+# Importing the Numpy library
+import numpy as np
+# Import the inverse function from the linear algebra library
+from numpy.linalg import inv
+
+# Declaring the 3x3 sample matrix
+a = np.array([[0, 2, 0], [1, 4, 2], [4, 2, 0]])
+# Declaring the 4x4 sample matrix
+b = np.array([[0, 2, 0, 8], [1, 4, 2, -2], [4, 2, 0, -3], [2, -1, -6, 5]])
+# Declaring the 5x5 sample matrix
+c = np.array([[0, 2, 0, 8, 1], [1, 2, 2, -2, 5], [4, 2, 0, -3, 2], [2, -1, -6, 5, 0], [-5, 3, 8, 4, 4]])
+# Declaring the nxn random matrix
+d = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 1, 2], [3, 4, 5, 6, 7, 8], [9, 0, 1, 2, 3, 4], [5, 6, 7, 8, 9, 0], [1, 2, 3, 4, 5, 6]])
+
+# Printing the sample matrix
+print ("Matrix:")
+print (a)
+# Printing the sample matrix inversed
+print ("Inverse Matrix:")
+print (inv(a))
+
+print ("Matrix:")
+print (b)
+print ("Inverse Matrix:")
+print (inv(b))
+
+print ("Matrix:")
+print (c)
+print ("Inverse Matrix:")
+print (inv(c))
+
+print ("Matrix:")
+print (d)
+print ("Inverse Matrix:")
+print (inv(d))
