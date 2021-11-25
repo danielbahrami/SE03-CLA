d90d1fcc5c2655de086dbc48e016f0849b3b0b25
Daniel Bahrami <daniel@bahrami.dk>
2021-11-26 00:44:51 +0100

    Solution

--- Assignment 2/solution.cpp
+++ Assignment 2/solution.cpp
@@ -7,13 +7,17 @@ double f(double x) {
     return -20 * pow(M_E, -(pow(x, 2) / 8)) - pow(M_E, (0.5 * cos(2 * M_PI * x))) + 20 + M_E;
 }
 
-double centralDifferences(double x, double h = 0.001) {
+double centralDifferences(double x) {
+    double h = 0.0001;
     return (f(x + h) - f(x - h)) / (2 * h);
 }
 
-double gradientDescent(double x, double e = 0.001, double iterMax = 100000, double a = 0.001) {
+double gradientDescent(double x) {
+    double a = 0.0001;
+    double e = 0.0001;
+    double iterMax = 100000;
     double count = 0;
-    while (fabs(centralDifferences(x)) > e && count < iterMax) {
+    while (abs(centralDifferences(x)) > e && count < iterMax) {
         x = x - a * centralDifferences(x);
         count++;
     }
@@ -21,6 +25,6 @@ double gradientDescent(double x, double e = 0.001, double iterMax = 100000, doub
 }
 
 int main() {
-    cout << gradientDescent(1) << endl;
+    cout << gradientDescent(1.0) << endl;
     return 0;
 }
