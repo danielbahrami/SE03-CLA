80a6c61327308daadf2749c6b33831a0e13cd0dc
Daniel Bahrami <daniel@bahrami.dk>
2021-11-29 01:07:14 +0100

    Commit

--- Assignment 2/solution.cpp
+++ Assignment 2/solution.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <cmath>
 
-using namespace std;
-
 double f(double x) {
     return -20 * pow(M_E, -(pow(x, 2) / 8)) - pow(M_E, (0.5 * cos(2 * M_PI * x))) + 20 + M_E;
 }
@@ -25,6 +23,6 @@ double gradientDescent(double x) {
 }
 
 int main() {
-    cout << gradientDescent(1) << endl;
+    std::cout << gradientDescent(1) << std::endl;
     return 0;
 }
