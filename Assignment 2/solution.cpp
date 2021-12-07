2720fe55a7d98a54e9adb2c0a5125357d55e7931
Daniel Bahrami <daniel@bahrami.dk>
2021-12-07 22:20:03 +0100

    Commit

--- Assignment 2/solution.cpp
+++ Assignment 2/solution.cpp
@@ -5,38 +5,39 @@ double f(double x) {
     return -20 * pow(M_E, -(pow(x, 2) / 8)) - pow(M_E, (0.5 * cos(2 * M_PI * x))) + 20 + M_E;
 }
 
-double centralDifferences(double x) {
+double firstDerivative(double x) {
     double h = 0.0001;
     return (f(x + h) - f(x - h)) / (2 * h);
 }
 
-std::string secondDerivative(double x) {
+double secondDerivative(double x) {
     double h = 0.0001;
-    double extrema = (f(x + 2 * h) - (2 * f(x)) + (f(x - 2 * h))) / (4 * pow(h, 2));
+    return (f(x + 2 * h) - (2 * f(x)) + (f(x - 2 * h))) / (4 * pow(h, 2));
+}
+
+void extrema(double x) {
+    double extrema = secondDerivative(x);
     if (extrema > 0) {
-        std::cout << extrema << std::endl;
-        return "Minimum";
+        std::cout << "Minimum" << std::endl;
     } else if (extrema < 0) {
-        std::cout << extrema << std::endl;
-        return "Maximum";
+        std::cout << "Maximum" << std::endl;
     }
-    return "";
 }
 
 double gradientDescent(double x) {
-    double a = 0.0001;
     double e = 0.0001;
     double iterMax = 100000;
     double count = 0;
-    while (fabs(centralDifferences(x)) > e && count < iterMax) {
-        x = x - a * centralDifferences(x);
+    while (fabs(firstDerivative(x)) > e && count < iterMax) {
+        x = x - (1 / secondDerivative(x)) * firstDerivative(x);
         count++;
     }
     return x;
 }
 
 int main() {
-    std::cout << secondDerivative(1) << std::endl;
-    std::cout << std::fixed << gradientDescent(1) << std::endl;
+    std::cout << gradientDescent(1) << std::endl;
+    double result = gradientDescent(1);
+    extrema(result);
     return 0;
 }
